e60274d7c89c41d39b2ea82ae9232362e75e2a12
iBelando <ignaciobelando@gmail.com>
2021-04-12 00:54:22 -0300

    3. Counter App

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,16 @@
+// import React, { Fragment } from 'react';
+import React from 'react';
+
+//  Functional Components
+const PrimeraApp = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
+
+}
+
+export default PrimeraApp;
